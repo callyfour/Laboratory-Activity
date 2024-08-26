4f00397168a85ba3425f1f4c7dc08ff84b3d57e0
Nousagiii <qjmpagtalunan@tip.edu.ph>
2024-08-26 21:25:26 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,16 @@
+package library;
 
+//public class book
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private String category;
+
+    // Constructor
+    public Book(String title, String author, String isbn, String category) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.category = category;
+    }

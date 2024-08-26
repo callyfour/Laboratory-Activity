bb4441f3910464ea4c53672974e9bc516a96058a
Karl Andre Porcal <kporcal@gmail.com>
2024-08-26 21:26:33 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -14,3 +14,13 @@ public class Book {
         this.isbn = isbn;
         this.category = category;
     }
+ // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+}
